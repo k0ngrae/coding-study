99748ce6d7b700408b34d9b5ca63fa0a9528a683
k0ngrae <phl7705@naver.com>
2023-01-11 18:04:30 +0900

    github

---
+++ app.js
@@ -0,0 +1,17 @@
+const calculator = {
+    add: function(a,b){
+        return a + b;
+    },
+    minus: function(a,b){
+        return a - b ;
+    },
+    multi: function(a,b){
+        return a * b ;
+    },
+    div: function(a,b){
+        return a / b;
+    }
+};
+
+const plusResult = calculator.multiple(14,22);
+console.log(plusResult);
